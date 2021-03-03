f1c8a56b363aed4124959a916ad2827f61c51cb4
Ezequias <ezequiasmoreira@hotmail.com>
2021-03-03 19:42:02 -0300

    Refatorado js

---
+++ js/form.js
@@ -0,0 +1,44 @@
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+    
+    var formulario = document.querySelector("#form-adiciona");
+    var paciente = obterPacienteDoFormulario(formulario);    
+    var pacienteTr = montaTr(paciente);   
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+    formulario.reset();
+});
+
+function obterPacienteDoFormulario(formulario){
+    var paciente = {
+        nome: formulario.nome.value,
+        peso: formulario.peso.value,
+        altura: formulario.altura.value,
+        gordura: formulario.gordura.value,
+        imc: calculaImc(formulario.peso.value,formulario.altura.value)
+    }
+    return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");   
+
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));    
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-nome"));
+
+    return pacienteTr;
+}
+
+function montaTd(dado,classe){
+    var td = document.createElement("td");
+    td.textContent = dado;
+    td.classList.add(classe);
+    return td;
+}
